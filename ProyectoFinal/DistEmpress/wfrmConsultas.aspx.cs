cf2c498a845ccc9de08566ac59e915be4ae9ef49
Jose <alfoeli03@hotmail.com>
2018-08-02 15:50:30 -0600

    Modified file wfrmConsultas. Changed menu

--- ProyectoFinal/DistEmpress/wfrmConsultas.aspx.cs
+++ ProyectoFinal/DistEmpress/wfrmConsultas.aspx.cs
@@ -18,17 +18,15 @@ namespace DistEmpress
             lbl_mensaje.Text = "";
             switch (Convert.ToChar(Session["perfil"]))
             {
-                case 'A':
-                    Ventas.Style.Add("display", "none");
-                    Compras.Style.Add("display", "none");
-                    break;
                 case 'C':
-                    Admin.Style.Add("display", "none");
-                    Compras.Style.Add("display", "none");
+                    usuario.Style.Add("display", "none");
+                    ingresoPedido.Style.Add("display", "none");
+                    consultas.Style.Add("display", "none");
                     break;
                 case 'V':
-                    Admin.Style.Add("display", "none");
-                    Compras.Style.Add("display", "none");
+                    usuario.Style.Add("display", "none");
+                    ingresoProducto.Style.Add("display", "none");
+                    consultas.Style.Add("display", "none");
                     break;
             }
         }
